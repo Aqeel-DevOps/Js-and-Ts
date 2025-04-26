aab3b134ebd1d19a02334753ee0702accf37cc44
Aqeel-DevOps <nomy.hope07@gmail.com>
2025-04-26 21:32:52 +0500

    updata

---
+++ operators/app.js
@@ -0,0 +1,29 @@
+// Maths Operators
+var currentYear = 2025;
+// let ageCalculator: number = currentYear - 2005;
+// console.log(ageCalculator);
+// console.log(2+4)
+// console.log(2-4)
+// console.log(2*4)
+// console.log(2/4)
+// console.log(2**4)
+// console.log(2%4)
+// let x : number = 10;
+// x = x+ 3 // x = x+1;
+// x++ // x = x+1;
+// x-- // x = x-1;
+// x = x*2; // x = x*2;
+// x *=2 // x = x*2;
+// x += 2; // x = x+2;
+// x -= 2; // x = x-2;
+// x /= 2; // x = x/2;
+// x **= 2; // x = x**2;
+// x %= 2; // x = x%2;
+// console.log(x);
+// Comparison Operators 
+// let a : number = 7;
+// let b : number = 5;
+// let c : boolean = a > b; // true
+// console.log(c);
+var checker = currentYear - 2000 > currentYear - 1947;
+console.log(checker); // true
---
+++ operators/app.ts
@@ -0,0 +1,40 @@
+// Maths Operators
+
+let currentYear : number = 2025;
+
+// let ageCalculator: number = currentYear - 2005;
+// console.log(ageCalculator);
+
+// console.log(2+4)
+// console.log(2-4)
+// console.log(2*4)
+// console.log(2/4)
+// console.log(2**4)
+// console.log(2%4)
+
+// let x : number = 10;
+// x = x+ 3 // x = x+1;
+// x++ // x = x+1;
+// x-- // x = x-1;
+// x = x*2; // x = x*2;
+// x *=2 // x = x*2;
+// x += 2; // x = x+2;
+// x -= 2; // x = x-2;
+// x /= 2; // x = x/2;
+// x **= 2; // x = x**2;
+// x %= 2; // x = x%2;
+
+// console.log(x);
+
+
+// Comparison Operators 
+
+// let a : number = 7;
+// let b : number = 5;
+// let c : boolean = a > b; // true
+
+// console.log(c);
+
+let checker : boolean = currentYear - 2000 > currentYear - 1947;
+console.log(checker); // true
+
