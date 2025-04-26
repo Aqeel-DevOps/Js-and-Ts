dfdb28ee843fd754e3b5ca4c3191d25acd8d53cf
Aqeel-DevOps <nomy.hope07@gmail.com>
2025-04-26 14:27:25 +0500

    its firrrst class data of js

---
+++ first class/app.js
@@ -0,0 +1,6 @@
+// npm install typescript -g (for globaly install)
+// to run app.ts file we use command tsc app.ts in cmd.
+// we use command for watching the file tsc app.ts -w to watch continuously.
+var firstName = "Nomi";
+firstName = "Nomi Khan";
+console.log(firstName);
---
+++ first class/app.ts
@@ -0,0 +1,9 @@
+
+// npm install typescript -g (for globaly install)
+// to run app.ts file we use command tsc app.ts in cmd.
+// we use command for watching the file tsc app.ts -w to watch continuously.
+
+
+let firstName: string = "Nomi";
+firstName = "Nomi Khan";
+console.log(firstName); 
